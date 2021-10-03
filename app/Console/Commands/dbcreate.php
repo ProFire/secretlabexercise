209d30213328735cfe464016809231f9e7dc9860
Shurn <shurnhg@sph.com.sg>
2021-10-03 14:05:06 +0800

    [New] Database creation

---
+++ app/Console/Commands/dbcreate.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Dear Secretlab Interviewer, this code is copied from https://www.techspeak.dev/2018/09/30/laravel-creating-the-database-using-artisan-commands.html
+ */
+class dbcreate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:create {name?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new MySQL database based on the database config file or the provided name';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $schemaName = $this->argument('name') ?: config("database.connections.mysql.database");
+        $charset = config("database.connections.mysql.charset",'utf8mb4');
+        $collation = config("database.connections.mysql.collation",'utf8mb4_unicode_ci');
+
+        config(["database.connections.mysql.database" => null]);
+
+        $query = "CREATE DATABASE IF NOT EXISTS $schemaName CHARACTER SET $charset COLLATE $collation;";
+
+        DB::statement($query);
+
+        config(["database.connections.mysql.database" => $schemaName]);
+
+    }
+}
